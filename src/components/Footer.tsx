f2b92220779aa3bc6b1b6761e4273f5cea5c8bcc
AshutoshARVR <m22ai810@iitj.ac.in>
2025-04-16 17:49:59 +0530

    name change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(): React.ReactElement {
   return (
     <footer className="text-center py-8 px-3 bg-black bg-opacity-70 text-white">
-      <p>&copy; 2025 Sys^3Labs. All rights reserved.</p>
+      <p>&copy; 2025 Company Labs. All rights reserved.</p>
     </footer>
   );
 }
